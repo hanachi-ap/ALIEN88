a52b52d2011ce996a85b976d103af6df82f1b0c2
hanachi-ap <hanachi.ap@gmail.com>
2023-01-26 18:04:18 +0900

    new feature: support gpc_rc

---
+++ firmware/qmk/qmk_firmware/keyboards/alien88/keymaps/gpk_rc/rules.mk
@@ -0,0 +1,9 @@
+# This file intentionally left blank
+VIA_ENABLE = yes
+# AUTO_SHIFT_ENABLE = yes
+BACKLIGHT_ENABLE = no
+RGBLIGHT_ENABLE = yes
+WS2812_DRIVER = vendor
+SRC += qmk_rc.c
+
+VIA_ENABLE = yes
